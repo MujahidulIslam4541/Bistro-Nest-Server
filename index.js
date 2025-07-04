d8d3131b0121e124a342f68a8a1b142cb2ec98c7
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-07-04 21:04:15 +0600

    create project structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("bistroNest is running");
+});
+app.listen(port, () => {
+  console.log(`bistroNest Project is running ${port}`);
+});
