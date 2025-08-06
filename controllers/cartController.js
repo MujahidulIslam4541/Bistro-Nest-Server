fdded44486e84c91fceb3f7ec287443b8eb384a1
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-08-07 01:10:02 +0600

    get all acrt data

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,17 +1,18 @@
 const client = require("../config/db");
 const cartCollection = client.db("bistroNestDb").collection("carts");
 
-
 // POST: Add item to cart
 exports.postCart = async (req, res) => {
-  const cartItem=req.body;
-  const result=await cartCollection.insertOne(cartItem)
+  const cartItem = req.body;
+  const result = await cartCollection.insertOne(cartItem);
   res.send(result);
 };
 
 // GET: Get all cart items
 
-exports.getCart=async(req,res)=>{
-    const result=await cartCollection.find().toArray()
-    res.send(result)
-}
+exports.getCart = async (req, res) => {
+  const email = req.query.email;
+  const query = { email: email };
+  const result = await cartCollection.find(query).toArray();
+  res.send(result);
+};
