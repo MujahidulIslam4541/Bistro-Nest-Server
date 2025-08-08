ce66bb19e5fcc2d42cb053602c31ebcf7dcd392c
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-08-08 21:06:57 +0600

    Save User if he doesn’t exists in the database

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,11 @@ const userCollection = client.db("bistroNestDb").collection("users");
 // POST: User information added to the database
 exports.postUser = async (req, res) => {
   const user = req.body;
+  const query = { email: user.email };
+  const existingUser = await userCollection.findOne(query);
+  if (existingUser) {
+    return res.send({ message: "user already exits", insertedId: null });
+  }
   const result = await userCollection.insertOne(user);
   res.send(result);
 };
