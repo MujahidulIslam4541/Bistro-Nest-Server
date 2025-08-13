27bd0ecf1a4d51f8fef0776db179b523af294767
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-08-13 21:10:20 +0600

    add item on server

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,7 +1,13 @@
-const client=require('../config/db')
-const menuCollection = client.db("bistroNestDb").collection('menu');
+const client = require("../config/db");
+const menuCollection = client.db("bistroNestDb").collection("menu");
 
-exports.getMenu=(async (req, res) => {
-      const result = await menuCollection.find().toArray();
-      res.send(result);
-    })
+exports.getMenu = async (req, res) => {
+  const result = await menuCollection.find().toArray();
+  res.send(result);
+};
+
+exports.menuPost = async (req, res) => {
+  const menu = req.body;
+  const result = await menuCollection.insertOne(menu);
+  res.send(result);
+};
--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,6 +1,9 @@
 const express=require('express')
 const router=express.Router();
-const {getMenu}=require('../controllers/menuController')
+const {getMenu, menuPost}=require('../controllers/menuController');
+const { verifyToken } = require('../controllers/jwtController');
+const { verifyAdmin } = require('../controllers/userController');
 
 router.get('/menu',getMenu);
+router.post('/menu', verifyToken,verifyAdmin, menuPost);
 module.exports=router;
