8dd8fefdfac91d64d8d6d38692dc3ed64e42d9e8
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-08-13 22:44:22 +0600

    delete menu

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const client = require("../config/db");
 const menuCollection = client.db("bistroNestDb").collection("menu");
 
@@ -9,5 +10,19 @@ exports.getMenu = async (req, res) => {
 exports.menuPost = async (req, res) => {
   const menu = req.body;
   const result = await menuCollection.insertOne(menu);
+  res.send(result);
+};
+
+exports.deleteMenu = async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await menuCollection.deleteOne(query);
+  res.send(result);
+};
+
+exports.getMenuById = async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await menuCollection.findOne(query);
   res.send(result);
 };
